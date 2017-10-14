b91677edf39d7d6c2b182a2104ce6ad433c3574c
Scott Weaver <scottmweaver@gmail.com>
2017-10-14 14:10:33 +0200

    Add CoffeeBreak base plugin.

---
+++ plugins/coffee-break/coffee-break.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: CoffeeBreak
+ * Plugin URI: https://github.com/humanmade/hm-coffee-break
+ * Description: Human Made Roulette
+ * Version: 0.0.1
+ * Author: Human Made
+ *
+ * @package coffee-break
+ */
+
+class Coffee_Break {
+	public function __construct() {
+		// Init
+	}
+}
+
+$coffee_break = new Coffee_Break();
