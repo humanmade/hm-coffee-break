8e95a7594044f3234282245668637bda5acee3a6
Chris Reynolds <chris@jazzsequence.com>
2017-10-20 13:46:26 -0600

    initialize options

--- plugins/coffee-break/inc/options.php
+++ plugins/coffee-break/inc/options.php
@@ -10,7 +10,38 @@
  */
 
 class Coffee_Break_Options {
+	public function __construct() {
+		add_action( 'cmb2_admin_init', [ $this, 'register_options_metabox' ] );
+	}
 
+	public function register_options_metabox() {
+		$prefix = '_cb_';
+
+		$options = new_cmb2_box( [
+			'id'           => $prefix . 'metabox',
+			'title'        => esc_html__( 'Coffee Break Options', 'hm-coffee-break' ),
+			'object_types' => [ 'options-page' ],
+			'option_key'   => $prefix . '_options',
+			'menu_title'   => esc_html__( 'Options', 'hm-coffee-break' ),
+			'parent_slug'  => 'edit.php?post_type=human',
+			'capability'   => 'manage_options',
+			'save_button'  => esc_html__( 'Save Options', 'hm-coffee-break' ),
+		] );
+
+		$options->add_field( [
+			'name'       => __( 'Oauth Token', 'hm-coffee-break' ),
+			'id'         => $prefix . 'oauth_token',
+			'type'       => 'text',
+			'desc'       => __( 'Oauth token for the Slackbot', 'hm-coffee-break' ),
+		] );
+
+		$options->add_field( [
+			'name'       => __( 'Bot Token', 'hm-coffee-break' ),
+			'id'         => $prefix . 'oauth',
+			'type'       => 'text',
+			'desc'       => __( 'Bot token for the Slackbot', 'hm-coffee-break' ),
+		] );
+	}
 
 }
 
