11aa46ec5136021c1cb94d58de9d9ccc5be02d39
Chris Reynolds <chris@jazzsequence.com>
2017-10-20 15:57:02 -0600

    adds phpunit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit1e59806971c4e2ef2de6cbce8151c581
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit1e59806971c4e2ef2de6cbce8151c581::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire1e59806971c4e2ef2de6cbce8151c581($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire1e59806971c4e2ef2de6cbce8151c581($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
